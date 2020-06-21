8e05ac53f6e49927d6242b870e7cf0f5b7795ff5
Krystof Rehacek <krystofee@gmail.com>
2020-06-21 14:36:53 +0200

    WIP

--- Appka.cpp
+++ Appka.cpp
@@ -5,23 +5,23 @@
 using namespace std;
 using namespace knihovna;
 
-int GRAF_1[S][S] = {{0, 1, 1, 0, 1}, {1, 0, 1, 0, 1}, {1, 1, 0, 1, 0}, {0, 0, 1, 0, 1}, {1, 1, 0, 1, 0}};
-int GRAF_2[S][S] = {{0, 1, 1, 0, 0}, {1, 0, 1, 0, 0}, {1, 1, 0, 1, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}};
-int GRAF_3[S][S] = {{0, 1, 0, 0, 0}, {1, 0, 0, 0, 0}, {0, 0, 0, 1, 1}, {0, 0, 1, 0, 1}, {0, 0, 1, 1, 0}};
+int GRAPHS[][S][S] = {{{0, 1, 1, 0, 1}, {1, 0, 1, 0, 1}, {1, 1, 0, 1, 0}, {0, 0, 1, 0, 1}, {1, 1, 0, 1, 0}},
+					  {{0, 1, 1, 0, 0}, {1, 0, 1, 0, 0}, {1, 1, 0, 1, 0}, {0, 0, 1, 0, 0}, {0, 0, 0, 0, 0}},
+					  {{0, 1, 0, 0, 0}, {1, 0, 0, 0, 0}, {0, 0, 0, 1, 1}, {0, 0, 1, 0, 1}, {0, 0, 1, 1, 0}}};
 
-void copy_graph(int result[S][S], int graf[S][S])
+void copy_graph(int result[S][S], int graph[S][S])
 {
 	for (int i = 0; i < S; i++)
 		for (int j = 0; j < S; j++)
-			result[i][j] = graf[i][j];
+			result[i][j] = graph[i][j];
 }
 
-void printgraf(const char *name, int graf[S][S])
+void print_graph(const char *name, int graph[S][S])
 {
 	cout << name << ":" << endl;
 	for (int i = 0; i < S; i++)
 		for (int j = 0; j < S; j++)
-			if (graf[i][j])
+			if (graph[i][j])
 				cout << " " << i << " -> " << j << endl;
 }
 
@@ -33,7 +33,7 @@ int get_choice()
 
 	while (!cin.good())
 	{
-		cout << "Spatny vstup." << '\n';
+		cout << "Spatny vstup." << endl;
 		cin.clear();
 		cin.ignore(INT_MAX, '\n');
 		cin >> setw(1) >> choice;
@@ -47,145 +47,127 @@ int get_choice()
 	return choice;
 }
 
-int main()
+void select_graph(int result[S][S])
 {
-	TList *top = NULL;
-	TList *head = NULL;
-	TList *tail = NULL;
-	TList *merena = NULL;
-	TList *nejvetsi = NULL;
+	print_graph("Graf 1", GRAPHS[0]);
+	print_graph("Graf 2", GRAPHS[1]);
+	print_graph("Graf 3", GRAPHS[2]);
 
-	int graf[S][S];
-	bool navstiveno[S];
+	cout << "Zvolte graph zadanim cisla." << endl;
 
-	int vybranygraf;
-	int algoritmus;
-	int start;
-	int konec;
+	int graph_index = get_choice();
+	bool selected = false;
 
-	printgraf("Graf 1", GRAF_1);
-	printgraf("Graf 2", GRAF_2);
-	printgraf("Graf 3", GRAF_3);
-
-	cout << "Zvolte graf zadanim cisla." << '\n';
-
-	vybranygraf = get_choice();
-
-	bool vybrano = false;
-	while (!vybrano)
+	while (!selected)
 	{
-		switch (vybranygraf)
+		if (graph_index >= 1 && graph_index <= 3)
 		{
-		case 1:
-			cout << "Vybran graf cislo 1." << '\n';
-			copy_graph(graf, GRAF_1);
-			vybrano = true;
-			break;
-		case 2:
-			cout << "Vybran graf cislo 2." << '\n';
-			copy_graph(graf, GRAF_2);
-			vybrano = true;
-			break;
-		case 3:
-			cout << "Vybran graf cislo 3." << '\n';
-			copy_graph(graf, GRAF_3);
-			vybrano = true;
-			break;
-		default:
-			cout << "Graf s timto cislem neexistuje!" << '\n';
-			vybranygraf = get_choice();
-			break;
+			cout << "Vybran graph cislo " << graph_index << "." << endl;
+			copy_graph(result, GRAPHS[graph_index]);
+			selected = true;
+		}
+		else
+		{
+			cout << "Co takhle si vybrat z grafu 1-3?" << endl;
+			graph_index = get_choice();
 		}
 	}
+}
 
-	cout << "Chcete: 1) najit komponentu s nejvetsim poctem vrcholu, nebo 2) zjistit existenci cesty mezi dvema vrcholy?" << '\n';
+void perform_algorithm(int algorithm, int graph[S][S])
+{
+	TList *top = NULL, *head = NULL, *tail = NULL, *merena = NULL, *nejvetsi = NULL;
 
-	// vyber algoritmus pres case
-	vybrano = false;
-	algoritmus = get_choice();
-	while (!vybrano)
+	bool navstiveno[S];
+	for (int i = 0; i < S; ++i)
+		navstiveno[i] = false;
+
+	int start = 0, konec = 0;
+	bool selected = false;
+
+	while (!selected)
 	{
-		switch (algoritmus)
+		switch (algorithm)
 		{
 		case 1:
-			cout << "Vybrano najiti komponenty s nejvetsim poctem vrcholu." << '\n';
-			vybrano = true;
-			//bigpart(graf);
+			cout << "Vybrano najiti komponenty s nejvetsim poctem vrcholu." << endl;
+			selected = true;
+			//bigpart(graph);
 			break;
 		case 2:
-			cout << "Vybrano zjisteni cesty mezi dvema vrcholy." << '\n';
-			vybrano = true;
+			cout << "Vybrano zjisteni cesty mezi dvema vrcholy." << endl;
+			selected = true;
 
-			cout << "Zadejte vrchol pro zacatek cesty." << '\n';
+			cout << "Zadejte vrchol pro zacatek cesty." << endl;
 			// vyber vrcholy pres case
-			vybrano = false;
+			selected = false;
 			start = get_choice();
-			while (!vybrano)
+			while (!selected)
 			{
 				switch (start)
 				{
 				case 1:
-					cout << "Vrchol 1 vybran jako start." << '\n';
-					vybrano = true;
+					cout << "Vrchol 1 vybran jako start." << endl;
+					selected = true;
 					break;
 				case 2:
-					cout << "Vrchol 2 vybran jako start." << '\n';
-					vybrano = true;
+					cout << "Vrchol 2 vybran jako start." << endl;
+					selected = true;
 					break;
 				case 3:
-					cout << "Vrchol 3 vybran jako start." << '\n';
-					vybrano = true;
+					cout << "Vrchol 3 vybran jako start." << endl;
+					selected = true;
 					break;
 				case 4:
-					cout << "Vrchol 4 vybran jako start." << '\n';
-					vybrano = true;
+					cout << "Vrchol 4 vybran jako start." << endl;
+					selected = true;
 					break;
 				case 5:
-					cout << "Vrchol 5 vybran jako start." << '\n';
-					vybrano = true;
+					cout << "Vrchol 5 vybran jako start." << endl;
+					selected = true;
 					break;
 
 				default:
-					cout << "Vrchol s timto cislem neexistuje!" << '\n';
+					cout << "Vrchol s timto cislem neexistuje!" << endl;
 					start = get_choice();
 					break;
 				}
 			}
-			cout << "Zadejte vrchol pro konec cesty." << '\n';
-			vybrano = false;
+			cout << "Zadejte vrchol pro konec cesty." << endl;
+			selected = false;
 			konec = get_choice();
-			while (!vybrano)
+			while (!selected)
 			{
 				switch (konec)
 				{
 				case 1:
-					cout << "Vrchol 1 vybran jako konec." << '\n';
-					vybrano = true;
+					cout << "Vrchol 1 vybran jako konec." << endl;
+					selected = true;
 					break;
 				case 2:
-					cout << "Vrchol 2 vybran jako konec." << '\n';
-					vybrano = true;
+					cout << "Vrchol 2 vybran jako konec." << endl;
+					selected = true;
 					break;
 				case 3:
-					cout << "Vrchol 3 vybran jako konec." << '\n';
-					vybrano = true;
+					cout << "Vrchol 3 vybran jako konec." << endl;
+					selected = true;
 					break;
 				case 4:
-					cout << "Vrchol 4 vybran jako konec." << '\n';
-					vybrano = true;
+					cout << "Vrchol 4 vybran jako konec." << endl;
+					selected = true;
 					break;
 				case 5:
-					cout << "Vrchol 5 vybran jako konec." << '\n';
-					vybrano = true;
+					cout << "Vrchol 5 vybran jako konec." << endl;
+					selected = true;
 					break;
 
 				default:
-					cout << "Vrchol s timto cislem neexistuje!" << '\n';
+					cout << "Vrchol s timto cislem neexistuje!" << endl;
 					konec = get_choice();
 					break;
 				}
 			}
-			if (findway(&top, graf, navstiveno, start - 1, konec - 1))
+			if (findway(&top, graph, navstiveno, start - 1, konec - 1))
 			{
 				cout << "Cesta mezi vrcholy " << start << " a " << konec << " existuje.";
 			}
@@ -196,9 +178,23 @@ int main()
 			break;
 
 		default:
-			cout << "Moznost s timto cislem neexistuje!" << '\n';
-			algoritmus = get_choice();
+			cout << "Moznost s timto cislem neexistuje!" << endl;
+			algorithm = get_choice();
 			break;
 		}
 	}
+}
+
+int select_algorithm()
+{
+	cout << "Chcete: 1) najit komponentu s nejvetsim poctem vrcholu, nebo 2) zjistit existenci cesty mezi dvema vrcholy?" << endl;
+	return get_choice();
+}
+
+int main()
+{
+	int graph[S][S];
+	select_graph(graph);
+	int algorithm = select_algorithm();
+	perform_algorithm(algorithm, graph);
 }
